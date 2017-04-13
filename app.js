eedaeb9727984081128d156a9d83528a7d4a8577
Elvis Oliveira <elvis.olv@gmail.com>
2017-04-13 11:15:43 -0300

    Encode fix. New component setup.

--- app.js
+++ app.js
@@ -6,26 +6,35 @@
             var dateArray = [];
             var date = moment().subtract(12, 'months').date(1);
             while (date <= moment()) {
-                dateArray.push( moment(date).format('YYYY-MM-DD') )
+                dateArray.push(moment(date).format('YYYY-MM-DD'));
                 date = moment(date).add(1, 'days');
             }
             return dateArray;
         }
     });
 
-    var AppComponent = ng.core.Component({
-        selector: 'my-app',
-        template: '<h1>Hello World!</h1>'
+    var CalendarComponent = ng.core.Component({
+        selector: 'calendar',
+        template: '<li>{{dates}}</li>'
     })
     .Class({
-        constructor: [CalendarService, function AppComponent(calendarService) {
-            console.log(calendarService.getCalendar());
+        constructor: [CalendarService, function CalendarComponent(calendarService) {
+            this.dates = calendarService.getCalendar();
         }]
+    });
+
+    var AppComponent = ng.core.Component({
+        selector: 'my-app',
+        template: '<h1>Hello World!</h1>' +
+                  '<calendar></calendar>'
+    })
+    .Class({
+        constructor: function AppComponent() {}
     });
 
     var AppModule = ng.core.NgModule({
         imports: [ng.platformBrowser.BrowserModule],
-        declarations: [AppComponent],
+        declarations: [AppComponent, CalendarComponent],
         providers: [CalendarService],
         bootstrap: [AppComponent]
     })
