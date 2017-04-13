508ebca1e924224302205978f6c39b0b3c36fe1b
Elvis Oliveira <elvis.olv@gmail.com>
2017-04-12 23:00:47 -0300

    Service implementation.

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 (function() {
 
+    var CalendarService = ng.core.Class({
+        constructor: function CalendarService() {},
+        getCalendar: function() {
+            var dateArray = [];
+            var date = moment().subtract(12, 'months').date(1);
+            while (date <= moment()) {
+                dateArray.push( moment(date).format('YYYY-MM-DD') )
+                date = moment(date).add(1, 'days');
+            }
+            return dateArray;
+        }
+    });
+
     var AppComponent = ng.core.Component({
         selector: 'my-app',
         template: '<h1>Hello World!</h1>'
     })
     .Class({
-        constructor: function() { }
+        constructor: [CalendarService, function AppComponent(calendarService) {
+            console.log(calendarService.getCalendar());
+        }]
     });
 
     var AppModule = ng.core.NgModule({
         imports: [ng.platformBrowser.BrowserModule],
         declarations: [AppComponent],
+        providers: [CalendarService],
         bootstrap: [AppComponent]
     })
     .Class({
